4e34d8d601642446ceb2278a7938161c813a4c4d
Virgilio <vbaez@bluecorona.com>
2022-06-12 10:22:05 -0400

    initial commit

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+import '../scss/main.scss';
+
+const toggleBtn = document.getElementById("nav-toggle");
+const homeBody = document.getElementById("home");
+const faIcon = document.getElementById("fa-icon");
+
+
+toggleBtn.onclick = function() {
+    homeBody.classList.toggle("menu-active");
+    faIcon.classList.toggle("fa-xmark");
+};
+
